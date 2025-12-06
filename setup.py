2d65a2d3668b80cccc4787585c07dd16612d8f45
ansh276 <aaransh27@gmail.com>
2025-12-06 15:41:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Ansh Arora",
+    author_email="aaransh27@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
